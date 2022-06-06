7f51dd186a5bd64a5c024d26def15accf5017698
mohit-wednesday <mohit.kushwaha@wednesday.is>
2022-06-06 15:44:17 +0530

    refactor: removed unused code

--- app/containers/App/tests/index.test.js
+++ app/containers/App/tests/index.test.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import { renderProvider, renderWithIntl } from '@utils/testUtils';
+import { renderWithIntl } from '@utils/testUtils';
 import App from '../index';
 import { BrowserRouter } from 'react-router-dom';
-import history from '@app/utils/history';
-import { waitFor } from '@testing-library/react';
 
 describe('<App /> container tests', () => {
   it('should render and match the snapshot', () => {
@@ -13,11 +11,5 @@ describe('<App /> container tests', () => {
       </BrowserRouter>
     );
     expect(container).toMatchSnapshot();
-  });
-
-  it('should redirect to redirect_uri query params if given in url', async () => {
-    history.location.search = '?redirect_uri=/repos';
-    renderProvider(<App />, history);
-    await waitFor(() => expect(history.location.pathname).toBe('/repos'));
   });
 });
