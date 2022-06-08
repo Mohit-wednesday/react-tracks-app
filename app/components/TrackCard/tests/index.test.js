15f909492ecf2c964165b9d21738ccc6d97caf32
mohit-wednesday <mohit.kushwaha@wednesday.is>
2022-06-08 14:16:30 +0530

    test: add test data and tests | update snapshots

--- app/components/TrackCard/tests/index.test.js
+++ app/components/TrackCard/tests/index.test.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
 /**
  *
  * Tests for TrackCard
@@ -5,27 +7,32 @@
  */
 
 import React from 'react';
-// import { fireEvent } from '@testing-library/dom'
-import { renderProvider } from '@utils/testUtils';
+import { fireEvent, waitFor } from '@testing-library/dom';
+import { renderProvider, timeout, createSpyOnAudio } from '@utils/testUtils';
 import TrackCard from '../index';
 import history from '@app/utils/history';
+import testData from '../testData';
 
 describe('<TrackCard />', () => {
   let handleOnClick = jest.fn();
-  const track = {
-    trackName: 'Sunflower',
-    artistName: 'Post Malone',
-    artworkUrl100: 'stub-image-url',
-    previewUrl: 'stub-preview-url',
-    trackId: 123
-  };
+  let playSpy;
+  let pauseSpy;
+
+  beforeAll(() => {
+    playSpy = createSpyOnAudio('play');
+    pauseSpy = createSpyOnAudio('pause');
+  });
 
   beforeEach(() => {
     handleOnClick = jest.fn();
   });
 
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   const MockTrackCard = () => {
-    return <TrackCard track={track} handleOnClick={handleOnClick} />;
+    return <TrackCard track={testData} handleOnClick={handleOnClick} />;
   };
 
   it('should render and match the snapshot', () => {
@@ -36,5 +43,43 @@ describe('<TrackCard />', () => {
   it('should contain 1 TrackCard component', () => {
     const { getAllByTestId } = renderProvider(<MockTrackCard />, history);
     expect(getAllByTestId('track-card').length).toBe(1);
+  });
+
+  it('should render trackName and artistName', () => {
+    const { getByTestId } = renderProvider(<MockTrackCard />, history);
+    expect(getByTestId('text-card')).toHaveTextContent(testData.artistName);
+    expect(getByTestId('text-card')).toHaveTextContent(testData.trackName);
+  });
+
+  it('should call handleOnClick and handleAudio callbacks accordingly', async () => {
+    let audio;
+    let handleAudioSpy = jest.fn();
+    const { getByTestId } = renderProvider(<MockTrackCard />, history);
+    audio = getByTestId('audio');
+    fireEvent.click(getByTestId('track-control-button'), { onClick: handleAudioSpy() });
+    await timeout(500);
+
+    expect(handleOnClick).toBeCalledWith({ current: audio });
+
+    fireEvent.click(getByTestId('track-control-button'), { onClick: handleAudioSpy() });
+    await timeout(500);
+
+    expect(handleAudioSpy).toBeCalled();
+    expect(handleOnClick).toBeCalledWith({ current: audio });
+  });
+
+  it('should play the audio if the play pause button clicked', async () => {
+    const { getByTestId } = renderProvider(<MockTrackCard />, history);
+    fireEvent.click(getByTestId('track-control-button'));
+    await waitFor(() => expect(playSpy).toHaveBeenCalledTimes(1));
+  });
+
+  it('should pause the playing audio if the play pause button clicked', async () => {
+    const { getByTestId } = renderProvider(<MockTrackCard />, history);
+    fireEvent.click(getByTestId('track-control-button'));
+    await waitFor(() => expect(playSpy).toHaveBeenCalledTimes(1));
+
+    fireEvent.click(getByTestId('track-control-button'));
+    await waitFor(() => expect(pauseSpy).toHaveBeenCalledTimes(1));
   });
 });
