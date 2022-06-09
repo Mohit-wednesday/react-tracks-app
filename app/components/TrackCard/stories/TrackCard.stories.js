2c41b351914d515d4f98f4d9723a5e1789f1e36d
mohit-wednesday <mohit.kushwaha@wednesday.is>
2022-06-09 18:35:23 +0530

    feat: track card story

--- app/components/TrackCard/stories/TrackCard.stories.js
+++ app/components/TrackCard/stories/TrackCard.stories.js
@@ -11,15 +11,10 @@ import { storiesOf } from '@storybook/react';
 import { text } from '@storybook/addon-knobs';
 import TrackCard from '../index';
 import { BrowserRouter } from 'react-router-dom';
+import testData from '../testData';
 
-const track = {
-  trackName: 'Sunflower',
-  artworkUrl100:
-    'https://is2-ssl.mzstatic.com/image/thumb/Music125/v4/4b/30/2c/4b302cb6-7a14-5464-4e97-0577e9d0be49/18UMGIM82277.rgb.jpg/100x100bb.jpg',
-  artistName: 'post malone'
-};
 storiesOf('TrackCard').add('simple', () => (
   <BrowserRouter>
-    <TrackCard id={text('id', 'TrackCard')} track={track} />
+    <TrackCard id={text('id', 'TrackCard')} track={testData} />
   </BrowserRouter>
 ));
